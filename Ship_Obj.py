5a070caa50c68d09709954219428c51a02a26b89
Bryce <themaughan1.0@gmail.com>
2020-12-02 16:57:44 -0700

    s

--- Ship_Obj.py
+++ Ship_Obj.py
@@ -37,14 +37,49 @@ class Ship(Object_Foundation.Object):
         self.angle = math.degrees(math.atan2(self.center.y, self.center.x))
         self.center.x = 300
         self.center.y = 200
-        self.velocity.dx = -math.sin(math.radians(self.angle)) * self.speed
-        self.velocity.dy = math.cos(math.radians(self.angle)) * self.speed
+        self.velocity.dx = -math.sin(self.angle) * self.speed
+        self.velocity.dy = math.cos(self.angle) * self.speed
+        
+        #self.change_x += -math.sin(self.radians) * speed
+        #self.change_y += math.cos(self.radians) * speed
 
     def advance(self):
-        #(math.radians(self.angle))
-        self.center.x += -self.speed * math.sin((math.radians(self.angle)))
-        self.center.y += self.speed * math.cos((math.radians(self.angle)))
-   
+        #(math.radians(self.angle)) * math.sin((math.radians(self.angle)))
+        self.center.x += -self.speed
+        self.center.y += self.speed
+    """
+    def rotate_point(x: float, y: float, cx: float, cy: float,
+                 angle_degrees: float) -> List[float]:
+    """
+    """
+    Rotate a point around a center.
+
+    :param x: x value of the point you want to rotate
+    :param y: y value of the point you want to rotate
+    :param cx: x value of the center point you want to rotate around
+    :param cy: y value of the center point you want to rotate around
+    :param angle_degrees: Angle, in degrees, to rotate
+    :return: Return rotated (x, y) pair
+    :rtype: (float, float)
+    """
+    """
+    temp_x = x - cx
+    temp_y = y - cy
+
+    # now apply rotation
+    angle_radians = math.radians(angle_degrees)
+    cos_angle = math.cos(angle_radians)
+    sin_angle = math.sin(angle_radians)
+    rotated_x = temp_x * cos_angle - temp_y * sin_angle
+    rotated_y = temp_x * sin_angle + temp_y * cos_angle
+
+    # translate back
+    rounding_precision = 2
+    x = round(rotated_x + cx, rounding_precision)
+    y = round(rotated_y + cy, rounding_precision)
+
+    return [x, y]
+    """
     def draw(self):
         self.sprite = arcade.load_texture("ship.png")
         arcade.draw_texture_rectangle(self.center.x, self.center.y, self.sprite.width / 2, self.sprite.height / 2, self.sprite, self.angle)
